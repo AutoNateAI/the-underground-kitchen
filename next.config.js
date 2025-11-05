adf5cca7c613b532b9ebf810842235f347764a25
Nathan Baker <nathan.baker@atomicobject.com>
2025-11-05 15:54:31 -0500

    Add assetPrefix for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/the-underground-kitchen',
+  assetPrefix: '/the-underground-kitchen',
   images: {
     unoptimized: true, // Required for static export
     domains: ['firebasestorage.googleapis.com'],
